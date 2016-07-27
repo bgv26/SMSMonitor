c57a98b68779d0d4603d9851b1ff713430b8687a
bgv26 <bgv26@mail.ru>
2016-07-28 00:02:46 +0300

    Small changes

--- app/src/main/java/com/brizhakgerman/smsmonitor/SmsContentProvider.java
+++ app/src/main/java/com/brizhakgerman/smsmonitor/SmsContentProvider.java
@@ -23,10 +23,6 @@ public class SmsContentProvider extends ContentProvider {
     private static final String BASE_PATH = "sms";
     public static final Uri CONTENT_URI = Uri.parse("content://"
             + AUTHORITY + "/" + BASE_PATH);
-    public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
-            + "/messages";
-    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
-            + "/message";
 
     private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
